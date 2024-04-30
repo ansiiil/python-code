2c88c89a820a6f211ab443f6606a014362d621b8
ansiiil <ansil095@gamil.com>
2024-04-30 06:53:09 +0530

    file printing

--- rd.py
+++ rd.py
@@ -1,9 +1,9 @@
-file=open("text.txt","a")
-def add():
-    name=input("enter name:")
-    adress=input("enter adress:")
-    phone=input("enter phone number:")
-    file.write("name:"+name+"\nadress:"+adress+"\n+phone:"+phone+"\n""___________________""\n")
-    print(name+"details adedd sucesfully")
-for a in range(3):
-    add()
+file=open("text.txt","w")
+a=int(input("enter a number:"))
+b=int(input("enter a second number:"))
+c=a+b
+d=a-b
+e=a*b
+f=a//b
+file.write("sum:"+str(c)+"\nsub:"+str(d)+"\nmul:"+str(e)+"\ndiv:"+str(f))
+print("writing sucessfuly")
